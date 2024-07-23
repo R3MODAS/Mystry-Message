8da25428242e31812d3492e58571ff5aaa8ffa44
R3MODAS <remodas7774@gmail.com>
2024-07-23 11:23:40 +0530

    feat: finished the basic nextauth setup along with middleware

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const Login = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()} className="bg-purple-500 text-white">
+          Sign out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>Login page</h1>
+      Not signed in <br />
+      <button
+        onClick={() => signIn()}
+        className="bg-purple-500 text-white px-5 py-1"
+      >
+        Sign in
+      </button>
+    </>
+  );
+};
+
+export default Login;
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -57,6 +57,12 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
@@ -77,12 +83,6 @@ export const authOptions: NextAuthOptions = {
 
       return session;
     },
-  },
-  pages: {
-    signIn: "/login",
-  },
-  session: {
-    strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
